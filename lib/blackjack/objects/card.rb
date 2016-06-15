2aef4a81cec540c385efb5ea9c6b67702259fae9
Hermann Krumrey <hermann@krumreyh.com>
2016-06-15 20:27:17 +0200

    Reformatted the card class.

--- lib/blackjack/objects/card.rb
+++ lib/blackjack/objects/card.rb
@@ -1,6 +1,6 @@
-#Copyright 2016 Hermann Krumrey
+# Copyright 2016 Hermann Krumrey
 #
-#This file is part of ruby-blackjack.
+# This file is part of ruby-blackjack.
 #
 #    ruby-blackjack is a program that lets a user play Blackjack using
 #    a command line interface.
@@ -19,76 +19,116 @@
 #    along with ruby-blackjack. If not, see <http://www.gnu.org/licenses/>.
 #
 
+require_relative('../strings/string.rb')
+
 # Class that models a playing card. It manages the value of the card in regards to
 # blackjack and generates ASCII art of the card
 class Card
 
-  # Creates a new card
-  # The constructor generates ASCII art of the card and calculates its blackjack value
-  def initialize(type, number)
 
-    top = '┌─────────┐'
-    bottom = '└─────────┘'
-    empty_row = "|         |\n"
-    suits = {'spades' => '♠', 'diamonds' => '♦', 'hearts' => '♥', 'clubs' => '♣'}
-    special_cards = {10 => '10', 12 => 'B', 13 => 'Q', 14 => 'K'}
+    # Creates a new card
+    # The constructor generates ASCII art of the card and calculates its blackjack value
+    # The card is also assigned a cardback.
+    # @param [string] type the type of the suit the card belongs to, i.e 'spades', 'hearts' etc.
+    # @param [int] number the number of the card. Numbers 2 through 10 are simply the normal numbers,
+    #                                             11 is the ace, 12 jack, 13 queen, 14 king
+    # @return [nil]
+    def initialize(type, number)
 
-    suit = suits[type]
-    @ace = false
-
-    if number < 10 and number != 1
-      @value = number
-      display_number = number.to_s
-    elsif number >= 10 and number != 11
-      @value = 10
-      display_number = special_cards[number]
-    else
-      @ace = true
-      @value = 11
-      display_number = 'A'
+      suit, @value, display_number, @ace = determine_card_data(type, number)
+      @backside = generate_cardback
+      @flipped = false
+      @ascii_card = format_card(suit, display_number)
     end
 
-    offset = 8 - display_number.length
 
-    @backside = self.class.generate_cardback
+    # Generates an ASCII playing card from the provided information
+    # @param [string] suit the suit of the card
+    # @param [string] number the number of the card (like 'A' for ace or '10' for 10)
+    # @return [string] the ASCII card
+    def format_card(suit, number)
 
-    @ascii_card = "#{top}\n| #{display_number}" + ' ' * offset + "|\n"
-    @ascii_card += empty_row * 2
-    @ascii_card += "|    #{suit}    |\n"
-    @ascii_card += empty_row * 2
-    @ascii_card += '|' + ' ' * offset + "#{display_number} |\n#{bottom}"
+        top =       '┌─────────┐'
+        empty_row = "|         |\n"
+        bottom =    '└─────────┘'
 
-  end
+        offset = 8 - number.length
 
-  def flip_over
-    cached = @ascii_card
-    @ascii_card = @backside
-    @backside = cached
-  end
+        ascii_card = "#{top}\n| #{number}" + ' ' * offset + "|\n"
+        ascii_card += empty_row * 2 + "|    #{suit}    |\n" + empty_row * 2
+        ascii_card += '|' + ' ' * offset + "#{number} |\n#{bottom}"
+        ascii_card.set_attributes([107, 30])  # BG = White, FG = Black
+    end
 
-  def get_ascii_card
-    @ascii_card
-  end
 
-  def get_value
-    @value
-  end
+    # Determines the suit of the card, its blackjack value, its number to be shown visually as well as
+    # if the card is an ace.
+    # @param [string] type the suit identifier string ('spades', 'hearts', 'clubs' or 'diamonds')
+    # @param [int] number the number of the card. Should be a number from 1 to 14
+    # @return [string, int, string, boolean] the suit of the card, the blackjack value of the card,
+    #                                        the string representation of the card number and if the card is an ace
+    def determine_card_data(type, number)
 
-  def is_ace
-    @ace
-  end
+        suits = {'spades' => '♠', 'diamonds' => '♦', 'hearts' => '♥', 'clubs' => '♣'}
+        special_cards = {10 => '10', 12 => 'B', 13 => 'Q', 14 => 'K'}
 
-  # Prints the card art to the console
-  def print_card
-    puts @ascii_card
-  end
+        ace = false
 
-  # Generates a cardback ASCII art
-  def self.generate_cardback
-    return colorize("┌─────────┐\n" + ("│░░░░░░░░░│\n" * 7) + '└─────────┘', 31)
-  end
+        suit = suits[type]
+        if type == 'spades' or type == 'clubs'
+          suit = suit.set_attributes([30], [107, 30]) # FG = Black, return to BG = White, FG = Black
+        else
+          suit = suit.set_attributes([31], [107, 30]) # FG = Red, return to BG = White, FG = Black
+        end
 
-  def self.colorize(string, mode)
-    return "\e[#{mode}m" + string.gsub!("\n", "\e[0m\n\e[#{mode}m") + "\e[0m"
-  end
+        if number < 10 and number != 1
+          value = number
+          display_number = number.to_s
+        elsif number >= 10 and number != 11
+          value = 10
+          display_number = special_cards[number]
+        else
+          ace = true
+          value = 11  # Set value of ace to 11, since we can then just downgrade the value to 1 if the need arises
+          display_number = 'A'
+        end
+
+        return suit, value, display_number, ace
+    end
+
+    # Generates a cardback ASCII art
+    # @return [string] the cardback ASCII art
+    def generate_cardback
+        ("┌─────────┐\n" + ("│░░░░░░░░░│\n" * 7) + '└─────────┘').set_attributes([107, 30]) # BG = White, FG = Black
+    end
+
+    # Flips over the card so that the cardback is shown instead of the generated ASCII card
+    # @return [nil]
+    def flip_over
+        cached = @ascii_card
+        @ascii_card = @backside
+        @backside = cached
+        @flipped = !@flipped
+    end
+
+    # @return [string] the generated ASCII card, or the cardback if the card is currently flipped
+    def get_ascii_card
+        @ascii_card
+    end
+
+    # @return [int] the blackjack value of the card
+    def get_value
+        @value
+    end
+
+    # @return [boolean] if the card is an ace or not
+    def is_ace
+        @ace
+    end
+
+    # @return [boolean] if the card is flipped or not
+    def is_flipped
+        @flipped
+    end
+
 end
