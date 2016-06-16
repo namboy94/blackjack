240c3322465f782e87754d8079442b3c28cb6772
Hermann Krumrey <hermann@krumreyh.com>
2016-06-16 11:35:54 +0200

    Allow failure on deploy phase

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,25 @@
+# Copyright 2016 Hermann Krumrey
+#
+# This file is part of ruby-blackjack.
+#
+#    ruby-blackjack is a program that lets a user play Blackjack using
+#    a command line interface.
+#
+#    ruby-blackjack is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    ruby-blackjack is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with ruby-blackjack. If not, see <http://www.gnu.org/licenses/>.
+#
+
 Rails.application.routes.draw do
   get 'home/index'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'home#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
